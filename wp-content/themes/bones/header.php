5ada96b10588efd27b875b206ad4276e1b6f7ad1
Corey Margulis <coreymargulis@gmail.com>
2014-11-11 16:20:03 -0600

    testtttttt

--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -53,7 +53,7 @@
 					<nav id="menu" role="navigation">
 						<ul>
 							<li><a href="#">Home</a></li>
-							<li><a href="#">About</a></li>
+							<li><a href="#">yessa</a></li>
 							<li><a href="#">Products</a></li>
 							<li><a href="#">Services</a></li>
 							<li><a href="#">Contact</a></li>
