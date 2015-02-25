23ffe89c5b7acdedf0e549eb6dfd83e0cfcfde55
Corey Margulis <coreymargulis@gmail.com>
2015-02-25 10:47:25 -0600

    rss attempt 3

--- wp-content/themes/karen/feed-karen.php
+++ wp-content/themes/karen/feed-karen.php
@@ -9,45 +9,45 @@
  */
 
 
-/**
- * Get related posts.
- */
-function my_rss_related() {
+// /**
+//  * Get related posts.
+//  */
+// function my_rss_related() {
 
-	global $post;
+// 	global $post;
 
-	// Setup post data
-	$pid     = $post->ID;
-	$tags    = wp_get_post_tags( $pid );
-	$tag_ids = array();
+// 	// Setup post data
+// 	$pid     = $post->ID;
+// 	$tags    = wp_get_post_tags( $pid );
+// 	$tag_ids = array();
 
-	// Loop through post tags
-	foreach ( $tags as $individual_tag ) {
-		$tag_ids[] = $individual_tag->term_id;
-	}
+// 	// Loop through post tags
+// 	foreach ( $tags as $individual_tag ) {
+// 		$tag_ids[] = $individual_tag->term_id;
+// 	}
 
-	// Execute WP_Query
-	$related_by_tag = new WP_Query( array(
-		'tag__in'          => $tag_ids,
-		'post__not_in'     => array( $pid ),
-		'posts_per_page'   => 3,
-	) );
+// 	// Execute WP_Query
+// 	$related_by_tag = new WP_Query( array(
+// 		'tag__in'          => $tag_ids,
+// 		'post__not_in'     => array( $pid ),
+// 		'posts_per_page'   => 3,
+// 	) );
 
-	// Loop through posts and build HTML
-	if ( $related_by_tag->have_posts() ) :
+// 	// Loop through posts and build HTML
+// 	if ( $related_by_tag->have_posts() ) :
 
-		echo 'Related:<br />';
+// 		echo 'Related:<br />';
 
-			while ( $related_by_tag->have_posts() ) : $related_by_tag->the_post();
-				echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a><br />';
-			endwhile;
+// 			while ( $related_by_tag->have_posts() ) : $related_by_tag->the_post();
+// 				echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a><br />';
+// 			endwhile;
 
-		else :
-			echo '';
-	endif;
+// 		else :
+// 			echo '';
+// 	endif;
 
-	wp_reset_postdata();
-}
+// 	wp_reset_postdata();
+// }
 
 
 /**
@@ -55,21 +55,21 @@ function my_rss_related() {
  */
 header( 'Content-Type: ' . feed_content_type( 'rss-http' ) . '; charset=' . get_option( 'blog_charset' ), true );
 $frequency  = 1;        // Default '1'. The frequency of RSS updates within the update period.
-$duration   = 'hourly'; // Default 'hourly'. Accepts 'hourly', 'daily', 'weekly', 'monthly', 'yearly'.
+// $duration   = 'hourly'; // Default 'hourly'. Accepts 'hourly', 'daily', 'weekly', 'monthly', 'yearly'.
 $postlink   = '<br /><a href="' . get_permalink() . '">See the rest of the story at mysite.com</a><br /><br />';
-$postimages = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'large' );
+// $postimages = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'large' );
 
-// Check for images
-if ( $postimages ) {
+// // Check for images
+// if ( $postimages ) {
 
-	// Get featured image
-	$postimage = $postimages[0];
+// 	// Get featured image
+// 	$postimage = $postimages[0];
 
-} else {
+// } else {
 
-	// Fallback to a default
-	$postimage = get_stylesheet_directory_uri() . '/images/default.jpg';
-}
+// 	// Fallback to a default
+// 	$postimage = get_stylesheet_directory_uri() . '/images/default.jpg';
+// }
 
 
 /**
@@ -99,13 +99,13 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 		<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
  
 		<!-- Feed Logo (optional) -->
-		<image>
+		<!-- <image>
 			<url>http://mysite.com/somelogo.png</url>
 			<title>
 				<?php bloginfo_rss( 'description' ) ?>
 			</title>
 			<link><?php bloginfo_rss( 'url' ) ?></link>
-		</image>
+		</image> -->
  
 		<?php do_action( 'rss2_head' ); ?>
 		
@@ -116,10 +116,9 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 				<title><?php the_title_rss(); ?></title>
 				<link><?php the_permalink_rss(); ?></link>
 				<guid isPermaLink="false"><?php the_guid(); ?></guid>
-				<author><?php the_author(); ?></author>
-				<image>
+				<!-- <image>
 					<url><?php echo esc_url( $postimage ); ?>"/></url>
-				</image>
+				</image> -->
 				<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true ), false ); ?></pubDate>
 				<content:encoded>
 					<![CDATA[<?php echo the_excerpt_rss(); echo $postlink; echo my_rss_related(); ?>]]>
