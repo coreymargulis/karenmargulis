6395ff6aa2efea8d942084e6309c401c9c00fe5a
Corey Margulis <coreymargulis@gmail.com>
2016-05-08 16:32:53 -0500

    a lot of things!

---
+++ wp-content/themes/karen new/index.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap">
+
+						<main id="main" class="">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+								<header class="article-header">
+
+									<p class="byline">
+										<?php printf( __( '', 'bonestheme' ).' %1$s',
+											/* the time the post was published */
+											'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+										); ?>
+									</p>
+
+									<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+								</header>
+
+								<section class="entry-content cf">
+									<?php the_content(); ?>
+								</section>
+
+							</article>
+
+							<?php endwhile; ?>
+
+									<?php bones_page_navi(); ?>
+
+							<?php else : ?>
+
+									<article id="post-not-found" class="hentry cf">
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+
+						</main>
+
+					<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
